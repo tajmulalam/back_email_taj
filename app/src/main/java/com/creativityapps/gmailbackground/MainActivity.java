bffaee6f9dc2fe61aab6ead9265dbc0c528a46ca
Md Tajmul Alam <tajmul.alam@multipletrading.net>
2018-10-10 19:06:45 +0600

    simple change

--- app/src/main/java/com/creativityapps/gmailbackground/MainActivity.java
+++ app/src/main/java/com/creativityapps/gmailbackground/MainActivity.java
@@ -7,11 +7,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.creativityapps.gmailbackgroundlibrary.BackgroundMail;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etToMail, etSubject, etBody;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +24,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        Button fab = (Button) findViewById(R.id.btnSend);
+        etToMail = (EditText) findViewById(R.id.etToMail);
+        etSubject = (EditText) findViewById(R.id.etSubject);
+        etBody = (EditText) findViewById(R.id.etBody);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendTestEmail();
+                sendTestEmail(etToMail.getText().toString(), etSubject.getText().toString(), etBody.getText().toString());
             }
         });
     }
@@ -51,16 +58,39 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void sendTestEmail(){
+    private void sendTestEmail(String mailTO, String subject, String body) {
         ///https://www.google.com/settings/security/lesssecureapps
         // go above link and turn it on other wise sender email will not be able to send mail[important]
-        BackgroundMail.newBuilder(this)
+        /*BackgroundMail.newBuilder(this)
                 .withUsername("tajmultipletrading@gmail.com")
                 .withPassword("Multiple312")
                 .withMailto("taj.cse37@gmail.com")
                 .withSubject("this is the subject")
                 .withBody("this is the body")
                 .withProcessVisibility(false)
+                .withOnSuccessCallback(new BackgroundMail.OnSuccessCallback() {
+                    @Override
+                    public void onSuccess() {
+                        //do some magic
+                        Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .withOnFailCallback(new BackgroundMail.OnFailCallback() {
+                    @Override
+                    public void onFail() {
+                        //do some magic
+                        Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .send();*/
+        BackgroundMail.newBuilder(this)
+                .withUsername("tajmultipletrading@gmail.com")
+                .withPassword("Multiple312")
+                .withMailto(mailTO)
+                .withSubject(subject)
+                .withBody(body)
+                .withProcessVisibility(true)
                 .withOnSuccessCallback(new BackgroundMail.OnSuccessCallback() {
                     @Override
                     public void onSuccess() {
